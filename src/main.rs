63a1045f979df98f06de870f06210fdd4258734c
Heorhi Valakhanovich <valahanovich@tut.by>
2020-01-31 00:37:53 +0300

    socks5: proof of concept

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 extern crate tokio;
 extern crate futures;
+extern crate trust_dns_resolver;
 
 pub(crate) mod util;
 mod tcppm;
 mod socks4;
+mod socks5;
 
 #[tokio::main]
 async fn main() {
     //tcppm(6666, "127.0.0.1:6667".parse().unwrap()).await;
-    socks4::socks4(6666).await;
+    socks5::socks5(6666).await;
 }

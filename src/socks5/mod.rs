cfc33b6581f04faf6fb65d9c5bcf8afe7859ab20
Heorhi Valakhanovich <valahanovich@tut.by>
2020-03-16 02:08:53 +0300

    socks5: refactor

---
+++ src/socks5/mod.rs
@@ -0,0 +1,147 @@
+use super::util;
+use crate::logger;
+use tokio::net::{TcpListener, TcpStream};
+
+use nom::{Err, IResult, Needed};
+use std::net::{IpAddr, SocketAddr};
+use tokio::io::{AsyncRead, AsyncReadExt};
+
+mod parser;
+
+enum Socks5Error {
+    Handshake,
+    InvalidAuth,
+    InvalidRequest,
+    TargetUnreachable,
+    Transceiver,
+}
+
+type Socks5Result<T> = Result<T, Socks5Error>;
+
+#[derive(Debug)]
+pub struct AuthRequest {
+    //VER
+    //NAUTH
+    pub auths: Vec<u8>,
+}
+
+pub enum RequestAddr {
+    Ip(IpAddr),
+    Domain(String),
+}
+pub struct ConnectRequest {
+    //VER
+    pub cmd: u8,
+    //RSV
+    pub addr: RequestAddr,
+    pub port: u16,
+}
+
+async fn parser_read<R, T, P>(stream: &mut R, parser: P) -> Option<T>
+where
+    R: AsyncRead + Unpin,
+    P: Fn(&[u8]) -> IResult<&[u8], T>,
+{
+    let mut temp = Vec::new();
+    loop {
+        match parser(&temp) {
+            Ok((_, result)) => return Some(result),
+            Err(Err::Incomplete(Needed::Size(size))) => {
+                let old_len = temp.len();
+                temp.resize(old_len + size, 0);
+                stream.read_exact(&mut temp[old_len..]).await.ok()?;
+            }
+            Err(Err::Incomplete(Needed::Unknown)) => {
+                let old_len = temp.len();
+                temp.resize(old_len + 1, 0);
+                stream.read_exact(&mut temp[old_len..]).await.ok()?;
+            }
+            _ => return None,
+        }
+    }
+}
+
+async fn socks5_parser(mut sock: TcpStream) -> Socks5Result<()> {
+    use tokio::io::AsyncWriteExt;
+    sock.set_nodelay(true).ok();
+    let auth_requeest = parser_read(&mut sock, parser::parse_auth)
+        .await
+        .ok_or(Socks5Error::Handshake)?;
+    if !auth_requeest.auths.contains(&0u8) {
+        //only support no auth
+        sock.write_all(&[0x5, 0xff]).await.ok();
+        return Err(Socks5Error::InvalidAuth);
+    }
+    //successful auth
+    sock.write_all(&[0x5, 0x0])
+        .await
+        .or(Err(Socks5Error::Handshake))?;
+    let request = parser_read(&mut sock, parser::parse_request)
+        .await
+        .ok_or(Socks5Error::InvalidRequest)?;
+    sock.write_all(&[0x5, 0x0, 0x0])
+        .await
+        .or(Err(Socks5Error::Handshake))?;
+    let sockaddr = match request.addr {
+        RequestAddr::Ip(addr) => SocketAddr::new(addr, request.port),
+        RequestAddr::Domain(domain) => {
+            let domain: String = domain.chars().filter(|c| *c != ':').collect();
+            let domain = format!("{}:{}", domain, request.port);
+            util::resolve_sockaddr(domain)
+                .await
+                .or(Err(Socks5Error::TargetUnreachable))?
+        }
+    };
+    let mut dest = TcpStream::connect(sockaddr)
+        .await
+        .or(Err(Socks5Error::TargetUnreachable))?;
+    let reply_addr = match sockaddr {
+        SocketAddr::V4(a) => {
+            let mut result = vec![1];
+            result.extend_from_slice(&a.ip().octets());
+            result.push((a.port() >> 8) as u8);
+            result.push(a.port() as u8);
+            result
+        }
+        SocketAddr::V6(a) => {
+            let mut result = vec![4];
+            result.extend_from_slice(&a.ip().octets());
+            result.push((a.port() >> 8) as u8);
+            result.push(a.port() as u8);
+            result
+        }
+    };
+    sock.write_all(&reply_addr)
+        .await
+        .or(Err(Socks5Error::Handshake))?;
+    logger::log(format!(
+        "socks5 {:?} -> {:?}",
+        sock.peer_addr().unwrap(),
+        dest.peer_addr().unwrap()
+    ));
+    util::transceiver(&mut sock, &mut dest)
+        .await
+        .or(Err(Socks5Error::Transceiver))?;
+    Ok(())
+}
+
+pub async fn socks5(src_port: u16) {
+    let mut listener = TcpListener::bind(("0.0.0.0", src_port)).await.unwrap();
+    loop {
+        let (sock, _addr) = listener.accept().await.unwrap();
+        tokio::spawn(async move { socks5_parser(sock).await.ok() });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn uniparser() {
+        let data = [5u8, 2, 0, 1];
+        let auth_req = parser_read(&mut &data[..], parser::parse_auth)
+            .await
+            .unwrap();
+        assert_eq!(auth_req.auths, [0, 1]);
+    }
+}
---
+++ src/socks5/parser.rs
@@ -0,0 +1,60 @@
+use super::{AuthRequest, ConnectRequest, RequestAddr};
+use nom::{
+    bytes::streaming::{tag, take},
+    error::{make_error, ErrorKind},
+    multi::count,
+    number::streaming::{be_u16, be_u8},
+    Err, IResult,
+};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
+pub fn parse_auth(input: &[u8]) -> IResult<&[u8], AuthRequest> {
+    let (rest, _) = tag([5u8])(input)?;
+    let (rest, len) = be_u8(rest)?;
+    let (rest, auths) = take(len)(rest)?;
+    let request = AuthRequest {
+        auths: Vec::from(auths),
+    };
+    Ok((rest, request))
+}
+
+pub fn parse_request(input: &[u8]) -> IResult<&[u8], ConnectRequest> {
+    let (rest, _) = tag([5u8])(input)?;
+    let (rest, cmd) = be_u8(rest)?;
+    let (rest_save, _rsv) = tag([0u8])(rest)?;
+    let (rest, addr_type) = be_u8(rest_save)?;
+    let (rest, addr) = match addr_type {
+        1 => {
+            let (rest, ip) = take(4usize)(rest)?;
+            (
+                rest,
+                RequestAddr::Ip(IpAddr::V4(Ipv4Addr::new(ip[0], ip[1], ip[2], ip[3]))),
+            )
+        }
+        3 => {
+            let (rest, len) = be_u8(rest)?;
+            let (rest, domain) = take(len)(rest)?;
+            (
+                rest,
+                RequestAddr::Domain(String::from_utf8_lossy(domain).into_owned()),
+            )
+        }
+        4 => {
+            let (rest, ip) = count(be_u16, 8)(rest)?;
+            (
+                rest,
+                RequestAddr::Ip(IpAddr::V6(Ipv6Addr::new(
+                    ip[0], ip[1], ip[2], ip[3], ip[4], ip[5], ip[6], ip[7],
+                ))),
+            )
+        }
+        _ => return Err(Err::Error(make_error(rest_save, ErrorKind::Verify))),
+    };
+    let (rest, dst_port) = be_u16(rest)?;
+    let request = ConnectRequest {
+        cmd,
+        addr,
+        port: dst_port,
+    };
+    Ok((rest, request))
+}

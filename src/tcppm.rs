da7e25dee23bb40fdcd84ada23ca7c34c1a7572a
Heorhi Valakhanovich <code@mail.geov.name>
2022-01-26 15:14:12 +0300

    remove dbg!

--- src/tcppm.rs
+++ src/tcppm.rs
@@ -4,14 +4,12 @@ use tokio::net::{TcpListener, TcpStream};
 
 pub async fn tcppm(name: String, src_port: u16, target: String) {
     let listener = TcpListener::bind(("0.0.0.0", src_port)).await.unwrap();
-    dbg!(&listener);
     loop {
         let (mut src, _addr) = listener.accept().await.unwrap();
         let name_clone = name.clone();
         let target_clone = target.clone();
         tokio::spawn(async move {
             if let Ok(dst_addr) = util::resolve_sockaddr(&target_clone).await {
-                dbg!(&dst_addr);
                 if let Ok(mut dst) = TcpStream::connect(dst_addr).await {
                     src.set_nodelay(true).ok();
                     dst.set_nodelay(true).ok();
